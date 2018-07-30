ed1d7363cc6ce1d35b1d84dc21a6b2a58d7c88ab
Alison Shin <alisonyshin@gmail.com>
2018-07-30 03:39:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,23 +4,12 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   else
-    array.[-1].insert(0,"and ")
-    and_last_word = "and " + array.pop
-    array << and_last_word
-    new_strings = array.join(", ")
-    return new_strings
+    array[-1].insert(0,"and ")
+    return array.join(", ")
   end
 end
 
 array = ["apple"]
 
 
-def oxford_comma(array)
-  if array.length == 2
-    return "#{array[0]} and #{array[1]}"
-  elsif 2 < array.length
-    array[-1].insert(0, "and ")
-  end
-  puts array.join(", ")    
-end
 oxford_comma(array)
