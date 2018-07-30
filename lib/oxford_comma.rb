fc744989cfcad425708d738ca9a54d1ae729b1ef
Alison Shin <alisonyshin@gmail.com>
2018-07-30 03:36:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,16 @@
-# def oxford_comma(array)
-#   if array.length == 1
-#     new_string = array[0].to_s
-#   elsif array.length == 2
-#     new_strings = array.join(" and ")
-#     return new_strings
-#   else
-#     and_last_word = "and " + array.pop
-#     array << and_last_word
-#     new_strings = array.join(", ")
-#     return new_strings
-#   end
-# end
+def oxford_comma(array)
+  if array.length == 1
+    new_string = array[0].to_s
+  elsif array.length == 2
+    new_strings = array.join(" and ")
+    return new_strings
+  else
+    and_last_word = "and " + array.pop
+    array << and_last_word
+    new_strings = array.join(", ")
+    return new_strings
+  end
+end
 
 array = ["apple"]
 
