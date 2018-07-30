84e89c7a6275488766569a311b7b18f18aca0e21
Alison Shin <alisonyshin@gmail.com>
2018-07-30 03:30:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
-    array
+    return array
   else
     and_last_word = "and " + array.pop
     array << and_last_word
     new_strings = array.join(", ")
+    return new_strings
   end
 end
 
