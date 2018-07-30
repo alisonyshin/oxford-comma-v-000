8e639ba125d70df47d7fbbd15db8d967577281ff
Alison Shin <alisonyshin@gmail.com>
2018-07-30 03:25:41 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
     puts array
   else
     last_word = "and " + array[-1].to_s
-    new_string = array.join(", ")
-    puts new_string
+    new_strings = array.join(", ")
+    puts new_strings
   end
 end
 
