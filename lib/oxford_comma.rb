9644525e6b2a338ef26295fa3c46351ea05acf0e
Alison Shin <alisonyshin@gmail.com>
2018-07-30 03:32:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
     return array.to_s
-  if array.length == 2
+  elsif array.length == 2
     new_strings = array.join(" and ")
     return new_strings
   else
