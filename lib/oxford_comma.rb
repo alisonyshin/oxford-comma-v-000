89e8459d8fb9816630e5ccec6be5fe9c17a83929
Alison Shin <alisonyshin@gmail.com>
2018-07-30 03:19:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     puts array
   else
+    last_word = array[-1]<<"and"
     array.insert(array.length-1, 'and')
     new_string = array.join(", ")
     puts new_string
